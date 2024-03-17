71d26666c1d9bd94a560a45f4c57825bc6b5d9f2
e <jannatul0040@gmail.com>
2024-03-17 13:09:08 +0600

    added sidebar

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar/Navbar";
+import Sidebar from "../components/Sidebar/Sidebar";
 
 
 const Layout = () => {
     return (
         <div className="w-screen h-screen relative">
             <Navbar></Navbar>
+            <Sidebar></Sidebar>
             <div className="md:pl-[250px] pl-[60px] pr-[20px] pt-[70px] w-full h-full overflow-y-auto">
                 <Outlet />
             </div>
